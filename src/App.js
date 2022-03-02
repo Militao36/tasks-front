c84d1d054c8a8326357c23b375f67317ac549b1c
Militao36 <mathuesmoreira3693@gmail.com>
2022-03-02 09:21:55 -0300

    COmmit

--- src/App.js
+++ src/App.js
@@ -1,85 +1,25 @@
-// import { Menu } from './components/Menu'
-// import { Project } from './components/Project';
-// import { Task } from './components/Task';
-import { TaskEdit } from './pages/TaskEdit';
-// import PageTasksBoard from './pages/TasksBoard';
+
+import { ProjectEdit } from './pages/ProjectEdit';
+import {
+  Route,
+  Routes,
+  BrowserRouter
+} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { PageTasksBoard } from './pages/TasksBoard';
+
+
 
 function App() {
-
   return (
-    <TaskEdit/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} exact />
+        <Route path="/projects/:id" element={<ProjectEdit />} exact />
+        <Route path="/tasks/board" element={<PageTasksBoard />} exact />
+      </Routes>
+    </BrowserRouter>
   )
-  // return (
-  //   <>
-  //     <Menu />
-  //     <div className="container-fluid mt-2 row" >
-  //       <div className='col-sm-10 col-md-8'>
-  //         <div className="row d-flex justify-content-evenly">
-  //           <div class="card text-white bg-success col-lg-3 mt-2" style={{ height: 100, minWidth: 250 }}>
-  //             <div class="card-body d-flex">
-  //               <p class="card-title" style={{ fontSize: '1.0em', marginRight: 20 }}>
-  //                 <i class="fa-solid fa-list-check" style={{ fontSize: "2em" }}></i>
-  //               </p>
-  //               <div>
-  //                 <p class="card-text">
-  //                   100
-  //                 </p>
-  //                 <p>Tarefas finalizadas</p>
-  //               </div>
-  //             </div>
-  //           </div>
-  //           <div class="card text-white bg-danger col-lg-3 mt-2" style={{ height: 100, minWidth: 250 }}>
-  //             <div class="card-body d-flex">
-  //               <p class="card-title" style={{ fontSize: '1.0em', marginRight: 20 }}>
-  //                 <i class="fa-solid fa-list-check" style={{ fontSize: "2em" }}></i>
-  //               </p>
-  //               <div>
-  //                 <p class="card-text">
-  //                   100
-  //                 </p>
-  //                 <p>Tarefas atrasadas</p>
-  //               </div>
-  //             </div>
-  //           </div>
-  //           <div class="card text-white bg-warning col-lg-3 mt-2" style={{ height: 100, minWidth: 250 }}>
-  //             <div class="card-body d-flex">
-  //               <p class="card-title" style={{ fontSize: '1.0em', marginRight: 20 }}>
-  //                 <i class="fa-solid fa-list-check" style={{ fontSize: "2em" }}></i>
-  //               </p>
-  //               <div>
-  //                 <p class="card-text">
-  //                   100
-  //                 </p>
-  //                 <p>Tarefas em espera</p>
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-
-  //         <Project />
-  //       </div>
-  //       <div className="col-sm-2 col-md-4">
-  //         <h5 className="card-title text-start">Tarefas em andamento</h5>
-  //         <Task
-  //           title={"Adicionar tela de cliente"}
-  //           status={"Finalizada"}
-  //           time={"Prazo: 01/02/2022"}
-  //           labels={[
-  //             {
-  //               name: 'Iniciado',
-  //               color: 'bg-primary'
-  //             },
-  //             {
-  //               name: 'Pausado',
-  //               color: 'bg-danger'
-  //             },
-
-  //           ]}
-  //         />
-  //       </div>
-  //     </div>
-  //   </>
-  // );
 }
 
 export default App;
--- src/pages/ProjectEdit.js
+++ src/pages/ProjectEdit.js
@@ -1,8 +1,42 @@
 import { Card } from "../components/Card";
 import { Editor } from "../components/Editor";
 import { Menu } from "../components/Menu";
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { api } from "../config/api";
 
-export function TaskEdit() {
+import { DateTime } from 'luxon'
+
+export function ProjectEdit() {
+  const { id } = useParams();
+
+  const [project, setProject] = useState({
+    id: "",
+    title: "",
+    description: "",
+    end_date: null,
+    start_date: null,
+    created_at: null,
+    updated_at: null,
+    users: []
+  })
+
+  useEffect(() => {
+    api.get(`http://localhost:3333/projects/${id}`)
+      .then(({ data }) => {
+        setProject({
+          id: data.id,
+          title: data.title,
+          description: data.description,
+          end_date: data.end_date,
+          start_date: data.start_date,
+          created_at: data.created_at,
+          updated_at: data.updated_at,
+          users: data.users
+        })
+      })
+  }, [id])
+
   return (
     <>
       <Menu />
@@ -27,7 +61,7 @@ export function TaskEdit() {
                   <div className="card shadow-sm">
                     <div className="card-body text-center">
                       <h5 className="card-title text-muted">Membros <i className="fa-solid fa-user-group text-success"></i></h5>
-                      <b className="card-text text-center text-muted mb-0">20 membros</b>
+                      <b className="card-text text-center text-muted mb-0">{project.users.length} membros</b>
                     </div>
                   </div>
                 </div>
@@ -35,7 +69,9 @@ export function TaskEdit() {
                   <div className="card shadow-sm">
                     <div className="card-body text-center">
                       <h5 className="card-title text-muted">Tempo gasto <i className="fa-solid fa-clock text-primary"></i></h5>
-                      <b className="card-text text-center text-muted mb-0">20 horas</b>
+                      <b className="card-text text-center text-muted mb-0">
+                        20 horas
+                      </b>
                     </div>
                   </div>
                 </div>
@@ -49,22 +85,22 @@ export function TaskEdit() {
                 </div>
               </div>
               <div className="mt-4">
-                <Editor />
+                <Editor value={project.description} />
                 <hr />
                 <div className="comments mt-4">
                   <div className="comment mb-4">
 
                     <form>
-                      <div class="mb-3">
-                        <label for="comment" class="form-label">Digite seu comentario ...</label>
-                        <textarea class="form-control form-control-sm" id="comment" rows="5"></textarea>
+                      <div className="mb-3">
+                        <label htmlFor="comment" className="form-label">Digite seu comentario ...</label>
+                        <textarea className="form-control form-control-sm" id="comment" rows="5"></textarea>
                       </div>
-                      <button type="submit" class="btn btn-sm btn-success">Comentar</button>
+                      <button type="submit" className="btn btn-sm btn-success">Comentar</button>
                     </form>
                   </div>
 
                   <div>
-                    <h4><i class="fa-solid fa-bars-staggered text-success"></i> Atividades</h4>
+                    <h4><i className="fa-solid fa-bars-staggered text-success"></i> Atividades</h4>
                   </div>
                   <div className="mt-4">
                     <div className="d-flex justify-content-between">
@@ -79,7 +115,7 @@ export function TaskEdit() {
 
                     <div className="d-flex justify-content-between">
                       <div className="d-flex justify-content-start">
-                        <a href="/" className="text-muted ms-2 decoratation"><i class="fa-solid fa-image"></i></a>
+                        <a href="/" className="text-muted ms-2 decoratation"><i className="fa-solid fa-image"></i></a>
                         <a href="/" className="text-muted ms-2 decoratation">@</a>
                       </div>
                       <div className="d-flex justify-content-end">
@@ -93,51 +129,35 @@ export function TaskEdit() {
             </div>
             <div className="col-sm-3">
               <div className="">
-                <h3 className="bg-success text-light p-3 rounded" ><i class="fa-brands fa-buffer"></i> Winasfit Web</h3>
+                <h3 className="bg-success text-light p-3 rounded" ><i className="fa-brands fa-buffer"></i> Winasfit Web</h3>
               </div>
 
               <div className="text-muted mt-3">
                 <p className="text-sm">Inicio
-                  <b className="d-block">01/02/2022</b>
+                  <b className="d-block">{project.start_date}</b>
                 </p>
                 <p className="text-sm mt-2">Fim
-                  <b className="d-block">Em desenvolvimento</b>
+                  <b className="d-block">{project.end_date || 'Em desenvolvimento'}</b>
                 </p>
               </div>
 
               <h5 className="mt-5 text-muted">
                 Lista de Membros
-                <i class="fa-solid fa-circle-plus text-success ms-2" style={{ cursor: 'pointer' }}></i>
+                <i className="fa-solid fa-circle-plus text-success ms-2" style={{ cursor: 'pointer' }}></i>
               </h5>
-              <ol class="list-group list-group-flush mt-3 mb-3">
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                  Matheus
-                  <i class="fa-solid fa-square-arrow-up-right text-dark" style={{ cursor: 'pointer' }}></i>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                  Canelão
-                  <i class="fa-solid fa-square-arrow-up-right text-dark" style={{ cursor: 'pointer' }}></i>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-start">
-                  Eriks
-                  <i class="fa-solid fa-square-arrow-up-right text-dark" style={{ cursor: 'pointer' }}></i>
-                </li>
+              <ol className="list-group list-group-flush mt-3 mb-3">
+                {project.users.map((user) => {
+                  return (
+                    <li key={user.id} className="list-group-item d-flex justify-content-between align-items-start">
+                      {user.username}
+                      <i className="fa-solid fa-square-arrow-up-right text-dark" style={{ cursor: 'pointer' }}></i>
+                    </li>
+                  )
+                })}
               </ol>
 
               <h5 className="mt-5 text-muted">Files</h5>
               <ul className="list-unstyled">
-                <li>
-                  <a href="/" className="btn-link text-secondary"><i className="far fa-fw fa-file-word"></i> Functional-requirements.docx</a>
-                </li>
-                <li>
-                  <a href="/" className="btn-link text-secondary"><i className="far fa-fw fa-file-pdf"></i> UAT.pdf</a>
-                </li>
-                <li>
-                  <a href="/" className="btn-link text-secondary"><i className="far fa-fw fa-envelope"></i> Email-from-flatbal.mln</a>
-                </li>
-                <li>
-                  <a href="/" className="btn-link text-secondary"><i className="far fa-fw fa-image "></i> Logo.png</a>
-                </li>
                 <li>
                   <a href="/" className="btn-link text-secondary"><i className="far fa-fw fa-file-word"></i> Contract-10_12_2014.docx</a>
                 </li>
